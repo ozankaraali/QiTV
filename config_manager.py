931e416d787d20e9c7dd07cb4f0db1352cbfc6eb
ozankaraali <ozan.karaali@gmail.com>
2024-06-25 17:08:22 +0200

    dependency updates and stuff, no features added

--- config_manager.py
+++ config_manager.py
@@ -5,7 +5,7 @@ import shutil
 
 
 class ConfigManager:
-    CURRENT_VERSION = "1.4.3"  # Set your current version here
+    CURRENT_VERSION = "1.4.4"  # Set your current version here
 
     def __init__(self):
         self.config = {}
