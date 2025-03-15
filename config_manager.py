202fc7a7b735afe32f13893cf153895fb52222ba
Berke Soysal <soysalberke.96@gmail.com>
2025-03-15 17:41:02 +0100

    fix the initialization of xml_channel_map

--- config_manager.py
+++ config_manager.py
@@ -295,7 +295,7 @@ class ConfigManager:
             "show_stb_content_info": ConfigManager.DEFAULT_OPTION_STB_CONTENT_INFO,
             "channel_logos": ConfigManager.DEFAULT_OPTION_CHANNEL_LOGO,
             "channel_epg": ConfigManager.DEFAULT_OPTION_CHANNEL_EPG,
-            "xmltv_channel_map": [],
+            "xmltv_channel_map": MultiKeyDict(),
             "max_cache_image_size": ConfigManager.DEFAULT_OPTION_MAX_CACHE_IMAGE_SIZE,
         }
 
